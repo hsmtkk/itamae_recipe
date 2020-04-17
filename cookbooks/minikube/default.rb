562ecf019cad016c63986b5b8f408692490360a3
a <a@b.com>
2020-04-16 23:32:15 -0400

    update

--- cookbooks/minikube/default.rb
+++ cookbooks/minikube/default.rb
@@ -1,5 +1,5 @@
 execute "curl -L -o /tmp/minikube.rpm https://storage.googleapis.com/minikube/releases/latest/minikube-1.9.1-0.x86_64.rpm" do
-  not_if "/tmp/minikube.rpm"
+  not_if "test -f /tmp/minikube.rpm"
 end
 
 execute "rpm -i /tmp/minikube.rpm" do
@@ -15,11 +15,13 @@ execute "curl -L -o /tmp/kubectl https://storage.googleapis.com/kubernetes-relea
 end
 
 execute "mv /tmp/kubectl /usr/local/bin/kubectl" do
-  not_if "/usr/local/bin/kubectl"
+  not_if "test -f /usr/local/bin/kubectl"
 end
 
 file "/usr/local/bin/kubectl" do
   action :nothing
   mode "0755"
 end
+
+execute "kubectl cluster-info"
 
