0eaa94b8246cf9478757d436a5dd73020bebcfaa
a <a@b.com>
2020-04-16 23:16:57 -0400

    add some conditions

--- cookbooks/minikube/default.rb
+++ cookbooks/minikube/default.rb
@@ -1,10 +1,22 @@
-execute "curl -L -o /tmp/minikube.rpm https://storage.googleapis.com/minikube/releases/latest/minikube-1.9.1-0.x86_64.rpm"
+execute "curl -L -o /tmp/minikube.rpm https://storage.googleapis.com/minikube/releases/latest/minikube-1.9.1-0.x86_64.rpm" do
+  not_if "/tmp/minikube.rpm"
+end
 
-execute "rpm -i /tmp/minikube.rpm"
+execute "rpm -i /tmp/minikube.rpm" do
+  not_if "rpm -q minikube"
+end
 
-execute "curl -L -o /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+execute "minikube start" do
+  user "admin"
+end
 
-execute "mv /tmp/kubectl /usr/local/bin/kubectl"
+execute "curl -L -o /tmp/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" do
+  not_if "test -f /tmp/kubectl"
+end
+
+execute "mv /tmp/kubectl /usr/local/bin/kubectl" do
+  not_if "/usr/local/bin/kubectl"
+end
 
 file "/usr/local/bin/kubectl" do
   action :nothing
