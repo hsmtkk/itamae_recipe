b9a41404b5b0dcd30b1772c36795a24a4074ea77
a <a@b.com>
2020-04-16 22:52:16 -0400

    install containerd.io manually

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -4,9 +4,10 @@ package "yum-utils"
 
 execute "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
 
+execute "yum install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm -y"
+
 package "docker-ce" 
 package "docker-ce-cli"
-package "containerd.io"
 
 service "docker" do
   action [:enable, :start]
